ac16f8f8ce81c6516bb28740759cbf07bca7191c
Boirokk <chadzx10@gmail.com>
2022-06-07 20:03:42 -0400

    Added Stars Rating

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -4,20 +4,20 @@ const catchAsync = require("../utils/catchAsync");
 const {isLoggedIn, isAuthor, validateCampground} = require('../middleware');
 const campgrounds = require("../controllers/campgrouinds");
 
-router.get('/', catchAsync(campgrounds.index));
+router.route('/')
+    .get(catchAsync(campgrounds.index))
+    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground));
 
 router.get('/new', isLoggedIn, campgrounds.renderNewForm);
 
-router.post('/', isLoggedIn, validateCampground, catchAsync(campgrounds.createNewCampground));
+router.route('/:id')
+    .get(catchAsync(campgrounds.showCampground))
+    .put(isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.edit))
+    .delete(isLoggedIn, isAuthor, catchAsync(campgrounds.delete));
 
-// Show Page
-router.get('/:id', catchAsync(campgrounds.showCampground));
 
 // Edit Form
 router.get('/:id/edit', isLoggedIn, isAuthor, catchAsync(campgrounds.showEditForm));
 
-router.put('/:id', isLoggedIn, isAuthor, validateCampground, catchAsync(campgrounds.edit));
-
-router.delete('/:id', isLoggedIn, isAuthor, catchAsync());
 
 module.exports = router;
